542201e6b863e84288790fec922ed3033ce22b67
Erin <erin001tw@gmail.com>
2023-08-30 22:11:04 +0800

    Converting Raw Addresses

--- VisualTon.py
+++ VisualTon.py
@@ -1,16 +1,32 @@
 import requests
 from datetime import datetime
 
+def get_readable_address(address):
+    url = f"https://toncenter.com/api/v2/packAddress?address={address}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if "result" in data:
+            return data["result"]
+        else:
+            return address
+    else:
+        return address
+
 def process_transaction(data):
     sender_address = data['account']['address']
+    sender_address_readable = get_readable_address(sender_address)
+    
     decoded_op_name = data['in_msg'].get('decoded_op_name')
 
     if decoded_op_name == "nft_ownership_assigned":
         receiver_address = data['in_msg']['source']['address']
+        receiver_address_readable = get_readable_address(receiver_address)
         amount = data['in_msg']['value']
         asset = "NFT"
     elif decoded_op_name == "jetton_notify":
         receiver_address = data['in_msg']['source']['address']
+        receiver_address_readable = get_readable_address(receiver_address)
         amount = int(data['in_msg']['decoded_body'].get('amount', 0)) // 10**9
         asset = "Jetton"
     else:
@@ -20,11 +36,11 @@ def process_transaction(data):
     confirm_time = data['utime']
     transaction_time = datetime.fromtimestamp(confirm_time).strftime('%Y-%m-%d %H:%M:%S')
 
-    print("Sender Address:", sender_address)
-    print("Receiver Address:", receiver_address)
+    print("Sender Address:", sender_address_readable)
+    print("Receiver Address:", receiver_address_readable)
     print(f"Amount: {amount} {asset}")
     print("Status:", status)
-    print("Confirm Time:", confirm_time)
+    print("Confirm Time:", transaction_time)
 
 transaction_id = input()
 url = f"https://tonapi.io/v2/blockchain/transactions/{transaction_id}"
@@ -40,19 +56,26 @@ if response.status_code == 200:
 
     if out_msgs:
         sender_address = data['account']['address']
+        sender_address_readable = get_readable_address(sender_address)
+        
         receiver_address = data['out_msgs'][0]['destination']['address']
+        receiver_address_readable = get_readable_address(receiver_address)
         amount = int(data['out_msgs'][0]['value']) // 10**9
         asset = "TON"
         status = "Success" if data['success'] else "Failure"
         confirm_time = data['utime']
         transaction_time = datetime.fromtimestamp(confirm_time).strftime('%Y-%m-%d %H:%M:%S')
 
-        print("Sender Address:", sender_address)
-        print("Receiver Address:", receiver_address)
+        print("Sender Address:", sender_address_readable)
+        print("Receiver Address:", receiver_address_readable)
         print(f"Amount: {amount} {asset}")
         print("Status:", status)
-        print("Confirm Time:", confirm_time)
+        print("Confirm Time:", transaction_time)
     else:
         process_transaction(data)
 else:
     print("Request failed with status code:", response.status_code)
+        
+#NFT: b811cf201eea9aa49b7648e709b1caf3c03c25f6c56d00e47eba52779999d717
+#TON: 0d90d5fc328a01b91074b8d8942be6e5b4901fe89fbcaa20667d1d0e85ca8160
+#Jetton: 87e48d95f4442c9fe42a502a380f4269a366d30a2e959985b000c2f878b0bd00
